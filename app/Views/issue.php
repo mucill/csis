4e127c7e34c9ce3e533f474f5663bd39388179de
unknown <hudasetiawan919@gmail.com>
2021-11-05 11:06:44 +0700

    update template

---
+++ app/Views/issue.php
@@ -0,0 +1,62 @@
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <h1>Daftar Issue</h1>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-md">
+                        <tr>
+                            <th>#</th>
+                            <th>Client</th>
+                            <th>Aplikasi</th>
+                            <th>Status</th>
+                            <th>Jenis</th>
+                            <th>Laporan</th>
+                            <th>Tanggal Buat</th>
+                            <th>Tanggal Update</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Irwansyah Saputra</td>
+                            <td>Morhuman</td>
+                            <td>
+                                <div class="badge badge-danger">Belum dikerjakan</div>
+                            </td>
+                            <td><a href="#" class="btn btn-secondary">Detail</a></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Hasan Basri</td>
+                            <td>Morbis</td>
+                            <td>
+                                <div class="badge badge-warning">dikerjakan</div>
+                            </td>
+                            <td><a href="#" class="btn btn-secondary">Detail</a></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Kusnadi</td>
+                            <td>Morbook</td>
+                            <td>
+                                <div class="badge badge-success">Selesai</div>
+                            </td>
+                            <td><a href="#" class="btn btn-secondary">Detail</a></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="section-body">
+            </div>
+    </section>
+</div>
